cfe0a19a1027cfb164aab2a3d373c67bdb1f2843
AlexSpirid <alexspirid384@gmail.com>
2022-10-20 12:08:56 +0300

    HW selenide#1

--- src/test/java/homework_selenide1/SearchJUnit5Test.java
+++ src/test/java/homework_selenide1/SearchJUnit5Test.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
@@ -16,13 +17,12 @@ public class SearchJUnit5Test {
 
     @Test
     void searchJUnit5Test() {
-        open("https://github.com/");
-        $("[data-test-selector=nav-search-input]").setValue("selenide").pressEnter();
-        $$(".repo-list li").first().$("a").click();
-        $("#repository-container-header").shouldHave(text("selenide / selenide"));
+        open("https://github.com/selenide/selenide");
         $("#wiki-tab").click();
-        $("#wiki-body").shouldHave(text("Soft assertions"));
-        $(Selectors.withText("Soft assertions")).click();
-        $(".markdown-body").shouldHave(Condition.text("Using JUnit5 extend test class:"));
+        $("#wiki-pages-filter").setValue("SoftAssertions");
+        $(".wiki-rightbar").shouldHave(text("SoftAssertions"));
+        $$(".details-reset").findBy(visible).shouldBe(text("SoftAssertions")).click();
+        $("#wiki-body").$(byText("3. Using JUnit5 extend test class:"))
+                .shouldHave(text("3. Using JUnit5 extend test class:"));
     }
 }
